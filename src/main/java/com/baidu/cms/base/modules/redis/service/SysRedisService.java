68fbed8b33a2e3076093872de6181d1c95649b6b
v_shiyanjun <v_shiyanjun@baidu.com>
2018-10-24 11:16:33 +0800

    解决缓存管理列表页读取list结构异常问题;

--- src/main/java/com/baidu/cms/base/modules/redis/service/SysRedisService.java
+++ src/main/java/com/baidu/cms/base/modules/redis/service/SysRedisService.java
@@ -1,10 +1,13 @@
 package com.baidu.cms.base.modules.redis.service;
 
+import com.alibaba.fastjson.JSON;
 import com.baidu.cms.base.modules.redis.entity.SysRedis;
+import com.baidu.cms.common.config.Global;
 import com.baidu.cms.common.persistence.Page;
 import com.baidu.cms.common.utils.RedisUtils;
 import com.baidu.cms.common.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
@@ -23,6 +26,9 @@ public class SysRedisService {
 
 	@Autowired
 	private RedisUtils redisUtils;
+
+	@Autowired
+	private ListOperations<String, Object> listOps;
 
 	public SysRedis get(String redisKey) {
 		String redisValue = redisUtils.get(redisKey);
@@ -55,11 +61,17 @@ public class SysRedisService {
 		Collections.sort(keyList);
 		page.setCount(keyList.size());
 		List<SysRedis> list = new ArrayList<>();
+		String value;
 		if (!CollectionUtils.isEmpty(keyList)) {
 			int start = pageNo == 1 ? 0 : (pageNo - 1) * pageSize;
 			for (int i = start, j = 0; i < keyList.size() && j < pageSize; i++, j++) {
 				String key = keyList.get(i);
-				String value = redisUtils.get(key);
+				if (key.startsWith(RedisUtils.prefix(Global.SYS_COLUMN_HIDE_LIST_KEY))) {
+					List<Object> range = listOps.range(key, 0, -1);
+					value = JSON.toJSONString(range);
+				} else {
+					value = redisUtils.get(key);
+				}
 				list.add(new SysRedis(RedisUtils.delPrefix(key), value));
 			}
 		}
